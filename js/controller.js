8959e0ae52c0e8401b16703e97e7f1742c35abc9
Tiffany Robinson <trobinson@CareKinesiss-MacBook-Pro-5.local>
2015-10-01 20:31:45 -0400

    removed for and if statements, but can probably still refactor more

--- js/controller.js
+++ js/controller.js
@@ -10,172 +10,95 @@
           // console.log("game: ", input);
           $rootScope.game = input;
 
-        //This creates the array of the number of rows
-
+          //This creates the array of the number of rows
           $scope.length= [];
-
-          for(var i=0; i < input.length; i++){
-             $scope.length.push(i);
-          }
+          $scope.length = _.range(input.length);
 
           //This creates the array of the number of columns
-
-          var array = [];
-
+          $scope.game.classes = [];
           $scope.width= [];
+
           var l="A";
 
-          for(var i=0; i < input.width; i++){
-             $scope.width.push(l);
-
-              //this mini loop will set the values for the ng-class
-             for(var j = 0; j < $scope.length.length; j++){
+          _.times(input.width, function(){
+            $scope.width.push(l);
+            var j = 0; 
+            _.times($scope.length.length, function(){
                 var thisClass= 'isActive' + $scope.length[j] + l;
-                array.push(thisClass);
+                $scope.game.classes.push(thisClass);
                 var model = $parse(thisClass);
                 model.assign($scope, false);
-             }
-
-             l = String.fromCharCode(l.charCodeAt(0) + 1);
-          }
-
-          $scope.game.classes= array;
+                j += 1;
+            });
+            l = String.fromCharCode(l.charCodeAt(0) + 1);
+          });
 
           //This sets the scope values for the rows and columns for the table
-
           $scope.game.rows = $scope.length;
           $scope.game.columns = $scope.width;
           $scope.value = false;
-        }
+        };
 
         //this allows the user to clear all cells
         $scope.clearAll= function(someObject){
-          for(var x = 0; x < someObject.classes.length; x++){
-            var thisClass = someObject.classes[x];
-
-            var model = $parse(thisClass);
+          _.each(someObject.classes, function(data){
+            var model = $parse(data);
             model.assign($scope, false); 
-          }  
-        }
-
+          });  
+        };
 
         //This will occur when a button is clicked 
         $scope.alert=function(location){
           var thisClass= "isActive" + location;
-
-          var currentValue = $scope[thisClass];
-
-          //toggle class 
-          currentValue = !currentValue;
-
           var model = $parse(thisClass);
-          model.assign($scope, currentValue); 
-
-       }
+          model.assign($scope, !($scope[thisClass])); 
+        };
 
        $scope.stop = function(someObject){
-        if($scope.promise != undefined){
           alert("You can't handle the truth!");
           $interval.cancel($scope.promise);
-        }
        }
 
        $scope.start = function(someObject){
-        // console.log("object: ", someObject);
-          // $interval( function(){
             var array = someObject.classes;
-            // for(var j = 0; j < someObject.iterations; j++){
-              // console.log("this is iteration number: ", j);
 
             $scope.promise = $interval(function(){
                 var needToUpdate = [];
-                // console.log("this is need to update: ", needToUpdate);
-                for(var i = 0; i < array.length; i++){
-                  var thisClass = array[i];
-                  var elements = thisClass.split("");
-                  var row = Number(elements[8]);
-                  var col = elements[9];
+                _.each(array, function(data){
+                    var thisClass = data;
+                    var elements = thisClass.split("");
+                    var row = Number(elements[8]);
+                    var col = elements[9];
 
-                  var top= "isActive" + (row-1) + col; 
-                  var bottom = "isActive" + (row + 1) + col;
-                  var topLeft = "isActive" + (row - 1) + String.fromCharCode(col.charCodeAt(0) - 1);
-                  var topRight = "isActive" + (row - 1) + String.fromCharCode(col.charCodeAt(0) + 1);
-                  var left = "isActive" + row + String.fromCharCode(col.charCodeAt(0) - 1);
-                  var right =  "isActive" + row + String.fromCharCode(col.charCodeAt(0) + 1);
-                  var bottomLeft = "isActive" + (row + 1) + String.fromCharCode(col.charCodeAt(0) - 1);
-                  var bottomRight = "isActive" + (row + 1) + String.fromCharCode(col.charCodeAt(0) + 1);
+                    var aliveNeighbors = _.filter([
+                        ("isActive" + (row-1) + col), 
+                        ("isActive" + (row + 1) + col), 
+                        ("isActive" + (row - 1) + String.fromCharCode(col.charCodeAt(0) - 1)), 
+                        ("isActive" + (row - 1) + String.fromCharCode(col.charCodeAt(0) + 1)),
+                        ("isActive" + row + String.fromCharCode(col.charCodeAt(0) - 1)),
+                        ("isActive" + row + String.fromCharCode(col.charCodeAt(0) + 1)),
+                        ("isActive" + (row + 1) + String.fromCharCode(col.charCodeAt(0) - 1)),
+                        ("isActive" + (row + 1) + String.fromCharCode(col.charCodeAt(0) + 1))
+                        ], function(data){
+                        return $scope[data] === true; 
+                    })
 
-                  var neighborsArray = []; 
+                   var singleArray = [];
+                   singleArray.push(thisClass);
 
-                  neighborsArray.push(top); 
-                  neighborsArray.push(bottom); 
-                  neighborsArray.push(topLeft); 
-                  neighborsArray.push(topRight); 
-                  neighborsArray.push(left); 
-                  neighborsArray.push(right); 
-                  neighborsArray.push(bottomLeft); 
-                  neighborsArray.push(bottomRight); 
+                   needToUpdate.push(_.filter(singleArray, function(data){
+                     return ($scope[data] === true && (aliveNeighbors.length < 2 || aliveNeighbors.length > 2)) || ($scope[data] === false && aliveNeighbors.length === 3);
+                   }).join())
 
-                  // console.log("these are the neighbors of: ", thisClass);
-                  // console.log("these are the neighbors: ", neighborsArray);
-
-                  var aliveNeighbors = 0; 
-
-                  for(var x = 0; x < neighborsArray.length; x++){
-                    var daClass = neighborsArray[x];
-                      if($scope[daClass] === true){
-                        aliveNeighbors += 1; 
-                      }
-                  }
-
-                  // console.log("alive neighbors: ", aliveNeighbors);
-
-                  if($scope[thisClass] === true){
-                    if(aliveNeighbors < 2){
-                      // console.log("this is what is ALIVE: ", thisClass);
-                      // console.log("this cell needs to be DEAD"); 
-                      needToUpdate.push(thisClass); 
-                    }
-                    else if( aliveNeighbors === 2 || aliveNeighbors === 2){
-                      // console.log("this cell needs to stay alive"); 
-                    }
-                    else if( aliveNeighbors > 3){
-                      // console.log("this is what is ALIVE: ", thisClass);
-                      // console.log("this cell needs to be DEAD"); 
-                      needToUpdate.push(thisClass); 
-                    }
-
-                  }
-
-                  if($scope[thisClass] === false){ //if the cell is dead
-                    if(aliveNeighbors === 3){
-                      // console.log("this is what is dead: ", thisClass);
-                      // console.log("this needs to become ALIVE");
-                      needToUpdate.push(thisClass);
-                    }
-                  }
-
-                }
-
-                if(needToUpdate.length === 0){
-                  alert("You have reached the end of your journey!");
-                  $interval.cancel($scope.promise);
-                }
-
-                for(var x = 0; x < needToUpdate.length; x++){
-                  // console.log("changing the cells: ", needToUpdate[x]);
-                  var thisClass = needToUpdate[x];
-                  var currentValue = $scope[thisClass];
-
-                  //toggle class 
-                  currentValue = !currentValue;
-
-                  var model = $parse(thisClass);
-                  model.assign($scope, currentValue); 
-                }  
-              }, 500, someObject.iterations);
-
-              // }
+                   needToUpdate = _.without(needToUpdate, "");
+                 });
+                
+                needToUpdate.length === 0 ? (alert("You have reached the end of your journey!"), $interval.cancel($scope.promise)) : 
+                    _.each(needToUpdate, function(data){
+                      var model = $parse(data);
+                      model.assign($scope, !($scope[data])); 
+                    });
+            }, 500, someObject.iterations);
        }
 
     });
